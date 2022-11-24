6c3c9615980285d4abdc196cc117701830798ca7
Jimmy <jimbotela@gmail.com>
2022-11-24 11:27:39 +0300

    modified app/database/session.py

--- app/database/session.py
+++ app/database/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:////Users/user/Documents/SirJayProjects/TechCamp/duka2.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///./duka2.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI, connect_args={'check_same_thread':False})
 
